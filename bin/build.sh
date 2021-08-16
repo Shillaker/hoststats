d62a022d0d35e91249d0744d7cb49d3dbce6c9c6
Simon Shillaker <scs17@ic.ac.uk>
2021-08-16 16:21:25 +0000

    Tweaks to scripts

--- bin/build.sh
+++ bin/build.sh
@@ -6,7 +6,7 @@ export VERSION=$(cat VERSION)
 TAG=shillaker/hoststats:${VERSION}
 
 export DOCKER_BUILDKIT=1
-docker build -t ${TAG} --build-arg VERSION=${VERSION} .
+docker build "$@" -t ${TAG} --build-arg VERSION=${VERSION} .
 
 docker push ${TAG}
 
--- bin/dist_tests.sh
+++ bin/dist_tests.sh
@@ -5,5 +5,9 @@ export VERSION=$(cat VERSION)
 # Run dist tests in client container
 docker-compose run --rm client nosetests hoststats.disttest --nocapture
 
+RETURN_CODE=$?
+
 # Print out all the logs
 docker-compose logs
+
+exit $RETURN_CODE

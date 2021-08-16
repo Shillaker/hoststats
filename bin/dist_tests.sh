7c7fedaa3d7c75489606020f96128eaa0305b801
Simon Shillaker <scs17@ic.ac.uk>
2021-08-16 13:28:35 +0000

    Added skeleton

--- bin/dist_tests.sh
+++ bin/dist_tests.sh
@@ -5,8 +5,11 @@ set -e
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 PROJ_ROOT="${THIS_DIR}/.."
 
+export DOCKER_BUILDKIT=1
+
 pushd ${PROJ_ROOT} >> /dev/null
 
-nosetests hoststats.tests --nocapture
+# Run dist tests in client container
+docker-compose run --rm client nosetests hoststats.disttest --nocapture
 
 popd >> /dev/null
---
+++ hoststats/disttest/test_distributed.py
@@ -0,0 +1,37 @@
+from os.path import exists
+from time import sleep
+from unittest import TestCase
+import pandas as pd
+import socket
+
+from hoststats.client import HostStats
+from hoststats.util.validation import validate_csv_data
+
+
+TEST_HOSTS = ["target-one", "target-two"]
+
+
+class TestHostStatsDistributed(TestCase):
+    def test_collection_with_ips(self):
+        ips = list()
+        for h in TEST_HOSTS:
+            addr_res = socket.gethostbyaddr(h)
+            ips.append(addr_res[2][0])
+
+        print(f"Detected test IPs: {ips}")
+
+        self.do_dist_checks(ips)
+
+    def test_collection_with_hostnames(self):
+        self.do_dist_checks(TEST_HOSTS)
+
+    def do_dist_checks(self, hosts):
+        s = HostStats(hosts)
+        s.start_collection()
+
+        sleep(5)
+
+        csv_path = "/tmp/hoststats.csv"
+        s.stop_and_write_to_csv(csv_path)
+
+        validate_csv_data(csv_path, hosts)

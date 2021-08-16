206874380ea8313bfa61a16424c55787777303da
Simon Shillaker <scs17@ic.ac.uk>
2021-08-16 16:16:24 +0000

    Add dc logs

--- bin/dist_tests.sh
+++ bin/dist_tests.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-set -e
 
 export VERSION=$(cat VERSION)
 
 # Run dist tests in client container
 docker-compose run --rm client nosetests hoststats.disttest --nocapture
+
+# Print out all the logs
+docker-compose logs

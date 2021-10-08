40f023b28fecfc8bbdbf308392a7bcf1924c747b
Carlos Segarra <carlos@carlossegarra.com>
2021-10-08 16:29:39 +0000

    add timeout to test

--- hoststats/collection.py
+++ hoststats/collection.py
@@ -2,13 +2,14 @@ import json
 import logging
 import sys
 import time
+from os import environ
 from time import sleep
 
 import psutil
 
 from hoststats.stats import ONE_MB, get_stats_dfs
 
-SLEEP_INTERVAL_SECS = 2
+SLEEP_INTERVAL_MS = int(environ.get("HOSTSTATS_SLEEP_INTERVAL_MS", "2000"))
 
 
 def collect_metrics(kill_queue, result_queue):
@@ -35,6 +36,7 @@ def collect_metrics(kill_queue, result_queue):
         cpu_stats[f"CPU_{cpu_num}_PCT_SYSTEM"] = list()
 
     while True:
+        logging.warn("TICK interval: {}".format(SLEEP_INTERVAL_MS))
         # Check queue to see if we need to finish
         if not kill_queue.empty():
             res = kill_queue.get_nowait()
@@ -130,4 +132,4 @@ def collect_metrics(kill_queue, result_queue):
         net_stats.loc[len(net_stats.index)] = net_row
 
         # Sleep until next measurement
-        sleep(SLEEP_INTERVAL_SECS)
+        sleep(float(SLEEP_INTERVAL_MS) / 1000)
--- hoststats/disttest/test_distributed.py
+++ hoststats/disttest/test_distributed.py
@@ -1,3 +1,4 @@
+import signal
 import socket
 from time import sleep
 from unittest import TestCase
@@ -6,6 +7,13 @@ from hoststats.client import HostStats
 from hoststats.validation import validate_csv_data
 
 TEST_HOSTS = ["target-one", "target-two", "target-three"]
+TIMEOUT_SEC = 5
+
+
+def timeout_handler(signum, frame):
+   print("Timeout hit!")
+   signal.alarm(0)
+   raise RuntimeError("Execution timed out")
 
 
 class TestHostStatsDistributed(TestCase):
@@ -33,13 +41,28 @@ class TestHostStatsDistributed(TestCase):
 
         self.do_dist_checks(TEST_HOSTS, proxy=proxy_ip)
 
-    def do_dist_checks(self, hosts, proxy=None):
+    def test_collection_long_run(self):
+        time_to_sleep = 20
+        print(
+            f"Test collecting results after sleeping for {time_to_sleep} seconds"
+        )
+
+        with self.assertRaises(RuntimeError):
+            self.do_dist_checks(TEST_HOSTS, seconds_to_sleep=time_to_sleep)
+
+    def do_dist_checks(self, hosts, proxy=None, seconds_to_sleep=5):
         s = HostStats(hosts, proxy=proxy)
         s.start_collection()
 
-        sleep(5)
+        sleep(seconds_to_sleep)
 
         csv_path = "/tmp/hoststats.csv"
+
+        print("stopping and writing...")
+        signal.signal(signal.SIGALRM, timeout_handler)
+        signal.alarm(TIMEOUT_SEC)
         s.stop_and_write_to_csv(csv_path)
+        print("deactivating alarm")
+        signal.alarm(0)
 
         validate_csv_data(csv_path, hosts)
